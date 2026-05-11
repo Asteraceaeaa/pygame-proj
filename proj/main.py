244071c3a6eaf3178c58aea315066925a978a50f
asterwace <kovinv454@gmail.com>
2026-05-12 03:35:43 +0500

    add Level Manager

--- proj/main.py
+++ proj/main.py
@@ -31,10 +31,13 @@ screen = pg.display.set_mode((WIDTH, HEIGHT))
 clock = pg.time.Clock()
 
 running = True
-state = 0
+state = 0 
+states = ["МЕНЮ", "ВЫБОР УРОВНЯ", ["Уровень 1", "Уровень 2", "Уровень 3"]]
+cur_level = 0
 while running:
 
     if state == 0:
+        print(f"Текущий статус: {states[state]}")
         menu = True
 
         options = ["START GAME", "EXIT"]
@@ -57,13 +60,14 @@ while running:
                         state = 1
                         if selected == 1:
                             running = False
+                            state = 0
                             print("Exit...")
                             
                             
 
             screen.fill("Black")
 
-            title = font_b.render("GAME", True, "White")
+            title = font_b.render(states[state], True, "White")
             title_rect = title.get_rect(center=(WIDTH//2, HEIGHT//4))
             screen.blit(title, title_rect)
 
@@ -75,10 +79,54 @@ while running:
 
             pg.display.flip()
             clock.tick(60)
-    if state == 1:
-        print("Game")
-        break
 
+    if state == 1:
+        print(f"Текущий статус: {states[state]}")
+
+        level_manager = True
+        options = ["1 Уровень", "2 Уровень", "3 Уровень", "Вернуться в меню"]
+        selected = 0
+        while level_manager:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    menu = False
+                    running = False
+                    print("Exit...")
+                
+                if event.type == pg.KEYDOWN:
+                    
+                    if event.key == pg.K_UP:
+                        selected -= 1 if selected != 0 else -3
+                    elif event.key == pg.K_DOWN:
+                        selected += 1 if selected != 3 else -3
+                    elif event.key == pg.K_RETURN:
+                        level_manager = False
+                        state = 2
+                        cur_level = selected if selected != 3 else 0
+                        
+                        if selected == 3:
+                            state = 0
+                            print("Exit to menu...")
+                            
+                        # print(selected)
+
+            screen.fill("Black")
+            if state != 1: break
+            title = font_b.render(states[state], True, "White")
+            title_rect = title.get_rect(center=(WIDTH//2, HEIGHT//4))
+            screen.blit(title, title_rect)
+
+            for i, option in enumerate(options):
+                color = "Yellow" if i == selected else "White"
+                text = font_s.render(option, True, color)
+                rect = text.get_rect(center=(WIDTH//2, HEIGHT//2.5 + i * 50))
+                screen.blit(text, rect)
+
+            pg.display.flip()
+            clock.tick(60)
+    if state == 2:
+        print(f"Выбрано: {states[state][cur_level]}")
+        break
 
 pg.quit()
 
